7ac0ebd87deec700400ae777f8aedb094f2afa6d
doaaa04 <124186473+doaaa04@users.noreply.github.com>
2023-05-08 22:24:37 +0300

    Task3.java
    Programming (java) 2 | chapter 7 task 3: Input/Output and Exception Handling
    solved by: Doaa H Alshawwa

--- Task3.java
+++ Task3.java
@@ -1,4 +1,7 @@
-
+/****
+Write a program that reads a file containing two columns of floating-point numbers.
+Prompt the user for the file name. Print the average of each column.
+*/
 
 package com.mycompany.task3;
 
